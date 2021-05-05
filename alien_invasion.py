9c3aeb6236d9a90d422f03cf70423b8f5c8c4a94
Gul Jamal Zim <guljamalzim@gmail.com>
2021-05-05 21:04:50 +0600

    Remove old bullets & extract _update_bullets method

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,9 +29,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            self._update_screen()            
-                        
+            self._update_bullets()
+            self._update_screen()
 
     def _check_events(self):
         """Watch for and respond to keyboard and mouse events."""
@@ -62,8 +61,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:        
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)  
+        
 
     def _quit_game(self):
         pygame.display.quit()
