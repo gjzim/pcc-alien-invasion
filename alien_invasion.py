fd8e82828837c7464115d691f57edf530e678352
Gul Jamal Zim <guljamalzim@gmail.com>
2021-06-06 12:14:06 +0600

    Add level info in the scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -129,6 +129,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
+
     def _update_aliens(self):
         """
         Check if the fleet is at an edge,
@@ -167,6 +171,7 @@ class AlienInvasion:
         self.stats.reset_stats()
         self.stats.game_active = True
         self.sb.prep_score()
+        self.sb.prep_level()
 
         # Get rid of any remaining aliens and bullets.
         self.aliens.empty()
